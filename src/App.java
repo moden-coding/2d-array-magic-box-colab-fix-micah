8820a1318677d41a44b3370aff56b3edc4b75ae4
MicahGann <micahgann3@gmail.com>
2026-01-16 12:49:04 -0500

    done

--- src/App.java
+++ src/App.java
@@ -26,7 +26,52 @@ public class App {
 	}
 	
 	public static boolean isMagic(int[][] array){
-		return false; //placeholder
-	}
+		for(int i=0;i<array.length;i++){
+            if(array[i].length != array.length){
+                return false; 
+            }
+        }
+        int Sum = 0;
+        for(int j=0;j<array.length;j++){
+            Sum += array[0][j];
+        }
+    
+        for(int i=0;i<array.length;i++){
+            int rowSum = 0;
+            for(int j=0;j<array.length;j++){
+                rowSum += array[i][j];
+            }
+            if(rowSum != Sum){
+                return false;
+            }
+        }
+        
+        for(int j=0;j<array.length;j++){
+            int colSum = 0;
+            for(int i=0;i<array.length;i++){
+                colSum += array[i][j];
+            }
+            if(colSum != Sum){
+                return false;
+            }
+        }
+        
+        int firstDiagSum = 0;
+        for(int i=0;i<array.length;i++){
+            firstDiagSum += array[i][i];
+        }
+        if(firstDiagSum != Sum){
+            return false;
+        }
+        
+        int secondDiagSum = 0;
+        for(int i=0;i<array.length;i++){
+            secondDiagSum += array[i][array.length-1-i];
+        }
+        if(secondDiagSum != Sum){
+            return false;
+        }
+        return true;
+    }
 	
 }
